7733fafb7f0036fa00f95dc6ff89fc2ed66b158d
Ken Barnhouse <Ken.Barnhouse@sas.com>
2018-12-04 09:33:29 -0500

    cleaned up the readability of the script

--- trigger_jenkins_build.sh
+++ trigger_jenkins_build.sh
@@ -4,4 +4,6 @@
 # this script assumes you have a .netrc entry to the jenkins server osijml01 setup already
 #
 
-curl -k -n -H "Jenkins-Crumb:c5338d1b4de6b570e67c5a1163b0b724" -X POST "https://osijml01.unx.sas.com/job/Run Ansible Playbook/buildWithParameters?ANSPLAYBOOK=check_mk_registration.yml&HOSTLIST=ossadm01.unx.sas.com"
+curl -k -n  \
+	-H "Jenkins-Crumb:c5338d1b4de6b570e67c5a1163b0b724" \
+	-X POST "https://jml01.unx.sas.com/job/Run Ansible Playbook/buildWithParameters?ANSPLAYBOOK=playbook.yml&HOSTLIST=test.barnhouse.com"
