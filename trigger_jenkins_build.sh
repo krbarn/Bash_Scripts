4677584e96d0cf211cd72f8d72890df2a1fec16e
Ken Barnhouse <Ken.Barnhouse@sas.com>
2019-04-24 13:09:34 -0400

    some minor updates

--- trigger_jenkins_build.sh
+++ trigger_jenkins_build.sh
@@ -5,5 +5,9 @@
 #
 
 curl -k -n  \
+<<<<<<< HEAD
 	-H "Jenkins-Crumb:XXXXXXXXXXXXXXXXX" \
+=======
+	-H "Jenkins-Crumb:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" \
+>>>>>>> some minor updates
 	-X POST "https://jenkins_master/job/Run Ansible Playbook/buildWithParameters?ANSPLAYBOOK=playbook.yml&HOSTLIST=test.barnhouse.com"
